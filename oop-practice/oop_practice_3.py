a4f82c153a814511c71599603a35dd6309777baf
Rishabh Tripathi <rishabhtripathi479@gmail.com>
2026-06-21 23:38:55 +0530

    Update oop_practice_3.py

--- oop-practice/oop_practice_3.py
+++ oop-practice/oop_practice_3.py
@@ -1,2 +1,17 @@
-# Day 7 - OOP Practice 3: dunder methods (__str__, __repr__)
+# OOP Practice 3: dunder methods (__str__, __repr__)
 
+class Employee:
+    no_of_employees=0
+    raise_amount=1.04
+    def __init__(self,first,last,pay):
+        self.first=first
+        self.last=last
+        self.pay=pay
+        self.email=first + '_' + last + '@company.com'
+        Employee.no_of_employees+=1
+
+    def full_name(self):
+        return "{} {}".format(self.first,self.last)
+
+    def apply_raise(self):
+        self.pay=int(self.pay * self.raise_amount)
